c63d75c663feefd97808e2180d77eed84b5ec6f3
Leslie Hoare <iam@lesleh.co.uk>
2024-02-09 14:44:52 +0000

    Add Route53 config for lesleh.co.uk

---
+++ main.tf
@@ -0,0 +1,7 @@
+module "lesleh_co_uk" {
+  source = "./lesleh_co_uk"
+}
+
+# module "jessca_co_uk" {
+#   source = "./jessca_co_uk"
+# }
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "domain" {
+  default = "test2.lesleh.co.uk"
+}
+
+variable "cert_domain" {
+  default = "*.lesleh.co.uk"
+}
+
+variable "domain_zone" {
+  default = "lesleh.co.uk"
+}
+
+variable "bucket_name" {
+  default = "test2.lesleh.co.uk"
+}
