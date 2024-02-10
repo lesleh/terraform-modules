9b45f9f538662b5cba7f5196cd0fa84af6cdea79
Leslie Hoare <iam@lesleh.co.uk>
2024-02-10 18:00:42 +0000

    Add public bucket

--- main.tf
+++ main.tf
@@ -2,6 +2,10 @@ module "lesleh_co_uk" {
   source = "./lesleh_co_uk"
 }
 
+module "public_storage" {
+  source = "./public_storage"
+}
+
 # module "jessca_co_uk" {
 #   source = "./jessca_co_uk"
 # }
---
+++ public_storage/main.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "leslehcouk_public_storage" {
+  bucket = "leslehcouk-public-storage"
+}
