158a90bc3f231cde90e0e7396f10bb6453dfaf73
Leslie Hoare <iam@lesleh.co.uk>
2024-02-17 20:35:46 +0000

    Add domain

--- lesleh_co_uk/main.tf
+++ lesleh_co_uk/main.tf
@@ -1,13 +1,44 @@
+resource "aws_route53domains_registered_domain" "lesleh_co_uk_registered_domain" {
+  domain_name = "lesleh.co.uk"
+
+  name_server {
+    glue_ips = []
+    name     = "ns-1832.awsdns-37.co.uk"
+  }
+  name_server {
+    glue_ips = []
+    name     = "ns-953.awsdns-55.net"
+  }
+  name_server {
+    glue_ips = []
+    name     = "ns-300.awsdns-37.com"
+  }
+  name_server {
+    glue_ips = []
+    name     = "ns-1132.awsdns-13.org"
+  }
+
+  registrant_contact {
+    extra_params = {
+      "UK_CONTACT_TYPE" = "IND"
+    }
+  }
+}
+
+//----------------------------------------------------------------------
+// AWS Route 53 records for lesleh.co.uk
+//----------------------------------------------------------------------
+
 resource "aws_route53_zone" "lesleh_co_uk_zone" {
-  name = "lesleh.co.uk"
+  name          = aws_route53domains_registered_domain.lesleh_co_uk_registered_domain.domain_name
   force_destroy = false
 }
 
 resource "aws_route53_record" "lesleh_co_uk_ns" {
   zone_id = aws_route53_zone.lesleh_co_uk_zone.id
-  name = "lesleh.co.uk"
-  type = "NS"
-  ttl = "172800"
+  name    = aws_route53domains_registered_domain.lesleh_co_uk_registered_domain.domain_name
+  type    = "NS"
+  ttl     = "172800"
   records = [
     "ns-1832.awsdns-37.co.uk.",
     "ns-953.awsdns-55.net.",
@@ -18,9 +49,9 @@ resource "aws_route53_record" "lesleh_co_uk_ns" {
 
 resource "aws_route53_record" "lesleh_co_uk_soa" {
   zone_id = aws_route53_zone.lesleh_co_uk_zone.id
-  name = "lesleh.co.uk"
-  type = "SOA"
-  ttl = "900"
+  name    = aws_route53domains_registered_domain.lesleh_co_uk_registered_domain.domain_name
+  type    = "SOA"
+  ttl     = "900"
   records = [
     "ns-1832.awsdns-37.co.uk. awsdns-hostmaster.amazon.com. 1 7200 900 1209600 86400"
   ]
@@ -32,7 +63,7 @@ resource "aws_route53_record" "lesleh_co_uk_soa" {
 
 resource "aws_route53_record" "lesleh_co_uk_spf_icloud_txt" {
   zone_id = aws_route53_zone.lesleh_co_uk_zone.id
-  name    = "lesleh.co.uk"
+  name    = aws_route53domains_registered_domain.lesleh_co_uk_registered_domain.domain_name
   type    = "TXT"
   ttl     = "300"
   records = [
@@ -43,9 +74,9 @@ resource "aws_route53_record" "lesleh_co_uk_spf_icloud_txt" {
 
 resource "aws_route53_record" "lesleh_co_uk_mx" {
   zone_id = aws_route53_zone.lesleh_co_uk_zone.id
-  name = "lesleh.co.uk"
-  type = "MX"
-  ttl = "300"
+  name    = aws_route53domains_registered_domain.lesleh_co_uk_registered_domain.domain_name
+  type    = "MX"
+  ttl     = "300"
   records = [
     "10 mx01.mail.icloud.com.",
     "10 mx02.mail.icloud.com."
@@ -54,9 +85,9 @@ resource "aws_route53_record" "lesleh_co_uk_mx" {
 
 resource "aws_route53_record" "lesleh_co_uk_cname__sig1" {
   zone_id = aws_route53_zone.lesleh_co_uk_zone.id
-  name = "sig1._domainkey.lesleh.co.uk"
-  type = "CNAME"
-  ttl = "300"
+  name    = "sig1._domainkey.lesleh.co.uk"
+  type    = "CNAME"
+  ttl     = "300"
   records = [
     "sig1.dkim.lesleh.co.uk.at.icloudmailadmin.com."
   ]
@@ -68,9 +99,9 @@ resource "aws_route53_record" "lesleh_co_uk_cname__sig1" {
 
 resource "aws_route53_record" "lesleh_co_uk_txt__amazonses_lesleh_co_uk" {
   zone_id = aws_route53_zone.lesleh_co_uk_zone.id
-  name = "_amazonses.lesleh.co.uk"
-  type = "TXT"
-  ttl = "1800"
+  name    = "_amazonses.lesleh.co.uk"
+  type    = "TXT"
+  ttl     = "1800"
   records = [
     "SfUKNWxWIGgdXvGmssLm2NmauWlIS1MXBRTG/SD0gyE="
   ]
@@ -78,9 +109,9 @@ resource "aws_route53_record" "lesleh_co_uk_txt__amazonses_lesleh_co_uk" {
 
 resource "aws_route53_record" "lesleh_co_uk_cname__fpnodtosjqt6lom4ssghrh2a76rsma2a" {
   zone_id = aws_route53_zone.lesleh_co_uk_zone.id
-  name = "fpnodtosjqt6lom4ssghrh2a76rsma2a._domainkey.lesleh.co.uk"
-  type = "CNAME"
-  ttl = "1800"
+  name    = "fpnodtosjqt6lom4ssghrh2a76rsma2a._domainkey.lesleh.co.uk"
+  type    = "CNAME"
+  ttl     = "1800"
   records = [
     "fpnodtosjqt6lom4ssghrh2a76rsma2a.dkim.amazonses.com"
   ]
@@ -88,9 +119,9 @@ resource "aws_route53_record" "lesleh_co_uk_cname__fpnodtosjqt6lom4ssghrh2a76rsm
 
 resource "aws_route53_record" "lesleh_co_uk_cname__r5wcjv3u2v4htdpjdsdtacbultxxc4xq" {
   zone_id = aws_route53_zone.lesleh_co_uk_zone.id
-  name = "r5wcjv3u2v4htdpjdsdtacbultxxc4xq._domainkey.lesleh.co.uk"
-  type = "CNAME"
-  ttl = "1800"
+  name    = "r5wcjv3u2v4htdpjdsdtacbultxxc4xq._domainkey.lesleh.co.uk"
+  type    = "CNAME"
+  ttl     = "1800"
   records = [
     "r5wcjv3u2v4htdpjdsdtacbultxxc4xq.dkim.amazonses.com"
   ]
@@ -98,9 +129,9 @@ resource "aws_route53_record" "lesleh_co_uk_cname__r5wcjv3u2v4htdpjdsdtacbultxxc
 
 resource "aws_route53_record" "lesleh_co_uk_cname__pircfq5ibpexkhsmvisodkkmbrwoymyk" {
   zone_id = aws_route53_zone.lesleh_co_uk_zone.id
-  name = "pircfq5ibpexkhsmvisodkkmbrwoymyk._domainkey.lesleh.co.uk"
-  type = "CNAME"
-  ttl = "1800"
+  name    = "pircfq5ibpexkhsmvisodkkmbrwoymyk._domainkey.lesleh.co.uk"
+  type    = "CNAME"
+  ttl     = "1800"
   records = [
     "pircfq5ibpexkhsmvisodkkmbrwoymyk.dkim.amazonses.com"
   ]
@@ -112,9 +143,9 @@ resource "aws_route53_record" "lesleh_co_uk_cname__pircfq5ibpexkhsmvisodkkmbrwoy
 
 resource "aws_route53_record" "lesleh_co_uk_cname___fc8e355558aadd2e111427c164f1e0bf" {
   zone_id = aws_route53_zone.lesleh_co_uk_zone.id
-  name = "_fc8e355558aadd2e111427c164f1e0bf.lesleh.co.uk"
-  type = "CNAME"
-  ttl = "300"
+  name    = "_fc8e355558aadd2e111427c164f1e0bf.lesleh.co.uk"
+  type    = "CNAME"
+  ttl     = "300"
   records = [
     "_2a1871b96968838b751ac31f67490282.tljzshvwok.acm-validations.aws."
   ]
@@ -122,9 +153,9 @@ resource "aws_route53_record" "lesleh_co_uk_cname___fc8e355558aadd2e111427c164f1
 
 resource "aws_route53_record" "lesleh_co_uk_cname___180821d5950a46c523572094c152e201" {
   zone_id = aws_route53_zone.lesleh_co_uk_zone.id
-  name = "_180821d5950a46c523572094c152e201.fns-dev.lesleh.co.uk"
-  type = "CNAME"
-  ttl = "300"
+  name    = "_180821d5950a46c523572094c152e201.fns-dev.lesleh.co.uk"
+  type    = "CNAME"
+  ttl     = "300"
   records = [
     "_6544caeedd9e845ffade431ce31911e4.jddtvkljgg.acm-validations.aws."
   ]
@@ -132,9 +163,9 @@ resource "aws_route53_record" "lesleh_co_uk_cname___180821d5950a46c523572094c152
 
 resource "aws_route53_record" "lesleh_co_uk_cname___7bdb23ae8bd898a3733cb7c30b4cbfd0" {
   zone_id = aws_route53_zone.lesleh_co_uk_zone.id
-  name = "_7bdb23ae8bd898a3733cb7c30b4cbfd0.fns.lesleh.co.uk"
-  type = "CNAME"
-  ttl = "300"
+  name    = "_7bdb23ae8bd898a3733cb7c30b4cbfd0.fns.lesleh.co.uk"
+  type    = "CNAME"
+  ttl     = "300"
   records = [
     "_06982a79059ce6c7c748259d5b2e5caf.jddtvkljgg.acm-validations.aws."
   ]
@@ -146,9 +177,9 @@ resource "aws_route53_record" "lesleh_co_uk_cname___7bdb23ae8bd898a3733cb7c30b4c
 
 resource "aws_route53_record" "lesleh_co_uk_a" {
   zone_id = aws_route53_zone.lesleh_co_uk_zone.id
-  name = "lesleh.co.uk"
-  type = "A"
-  ttl = "300"
+  name    = aws_route53domains_registered_domain.lesleh_co_uk_registered_domain.domain_name
+  type    = "A"
+  ttl     = "300"
   records = [
     "76.76.21.21"
   ]
@@ -156,20 +187,20 @@ resource "aws_route53_record" "lesleh_co_uk_a" {
 
 resource "aws_route53_record" "lesleh_co_uk_a__ai" {
   zone_id = aws_route53_zone.lesleh_co_uk_zone.id
-  name = "ai.lesleh.co.uk"
-  type = "A"
+  name    = "ai.lesleh.co.uk"
+  type    = "A"
   alias {
-    name = "d-1ji18q3it7.execute-api.eu-west-2.amazonaws.com."
-    zone_id = "ZJ5UAJN8Y3Z2Q"
+    name                   = "d-1ji18q3it7.execute-api.eu-west-2.amazonaws.com."
+    zone_id                = "ZJ5UAJN8Y3Z2Q"
     evaluate_target_health = true
   }
 }
 
 resource "aws_route53_record" "lesleh_co_uk_cname__conversational" {
   zone_id = aws_route53_zone.lesleh_co_uk_zone.id
-  name = "conversational.lesleh.co.uk"
-  type = "CNAME"
-  ttl = "300"
+  name    = "conversational.lesleh.co.uk"
+  type    = "CNAME"
+  ttl     = "300"
   records = [
     "cname.vercel-dns.com."
   ]
@@ -177,9 +208,9 @@ resource "aws_route53_record" "lesleh_co_uk_cname__conversational" {
 
 resource "aws_route53_record" "lesleh_co_uk_cname__quizzical" {
   zone_id = aws_route53_zone.lesleh_co_uk_zone.id
-  name = "quizzical.lesleh.co.uk"
-  type = "CNAME"
-  ttl = "300"
+  name    = "quizzical.lesleh.co.uk"
+  type    = "CNAME"
+  ttl     = "300"
   records = [
     "cname.vercel-dns.com."
   ]
@@ -187,9 +218,9 @@ resource "aws_route53_record" "lesleh_co_uk_cname__quizzical" {
 
 resource "aws_route53_record" "lesleh_co_uk_cname__reader" {
   zone_id = aws_route53_zone.lesleh_co_uk_zone.id
-  name = "reader.lesleh.co.uk"
-  type = "CNAME"
-  ttl = "300"
+  name    = "reader.lesleh.co.uk"
+  type    = "CNAME"
+  ttl     = "300"
   records = [
     "cname.vercel-dns.com."
   ]
@@ -197,9 +228,9 @@ resource "aws_route53_record" "lesleh_co_uk_cname__reader" {
 
 resource "aws_route53_record" "lesleh_co_uk_cname__www" {
   zone_id = aws_route53_zone.lesleh_co_uk_zone.id
-  name = "www.lesleh.co.uk"
-  type = "CNAME"
-  ttl = "300"
+  name    = "www.lesleh.co.uk"
+  type    = "CNAME"
+  ttl     = "300"
   records = [
     "cname.vercel-dns.com."
   ]
@@ -207,9 +238,9 @@ resource "aws_route53_record" "lesleh_co_uk_cname__www" {
 
 resource "aws_route53_record" "lesleh_co_uk_a__home" {
   zone_id = aws_route53_zone.lesleh_co_uk_zone.id
-  name = "home.lesleh.co.uk"
-  type = "A"
-  ttl = "60"
+  name    = "home.lesleh.co.uk"
+  type    = "A"
+  ttl     = "60"
   records = [
     "78.151.97.112"
   ]
