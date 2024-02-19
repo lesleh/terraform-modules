04e196c69a57795e49f6db801203adaacb23dc2b
Leslie Hoare <iam@lesleh.co.uk>
2024-02-19 10:26:52 +0000

    Move files around

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+
+  backend "s3" {
+    bucket = "leslehcouk-terraform-states"
+    key    = "terraform-modules"
+    region = "eu-west-2"
+  }
+}
+
 module "lesleh_co_uk" {
   source = "./lesleh_co_uk"
 }
