571df79a8ae08d33b416f0849b48052acf106335
Leslie Hoare <iam@lesleh.co.uk>
2024-09-24 10:02:57 +0100

    Add lambda_function_url output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "lesleh_co_uk_route53_zone_id" {
   value = module.lesleh_co_uk.zone_id
 }
+
+output "current_time_lambda_function_url" {
+  value = module.current_time.lambda_function_url
+}
